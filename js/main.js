062181c64bcccee316f45c4b4cd676a933cae729
Vika22 <vika.tkachenko22@gmail.com>
2020-03-23 13:18:58 +0200

    do

--- js/main.js
+++ js/main.js
@@ -1,13 +1,6 @@
-$(function () {
-    $('.slider-speech').slick({
-        nextArrow: '<button type="button" class="slick-btn slick-next"><i class="right"></i></button>',
-        prevArrow: '<button type="button" class="slick-btn slick-prev"><i class="left"></i></button>',
-        dots: false
-    });
-
-});
 var header = document.getElementById("active-div");
 var btns = header.getElementsByClassName("service-item");
+var arrow = header.querySelectorAll(".down");
 for (var i = 0; i < btns.length; i++) {
     btns[i].addEventListener("click", function () {
         var current = document.getElementsByClassName("active");
